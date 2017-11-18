971f71e311666816a9513279fbd8fb1fe0964f73
Martin Darmüntzel <martin@trivialanalog.de>
2017-11-18 13:01:02 +0100

    Erweitert den Testquellcode.

--- Modula2/hello.m
+++ Modula2/hello.m
@@ -7,8 +7,26 @@ CONST
 	pi = 3.1415926;
 
 VAR
-	x : REAL;
+	x : INTEGER;
+	s: STRING;
+	c :CHAR;
+	d:CHAR;
+	arr:ARRAY[0..15]OF INTEGER;
 
 BEGIN
 	f := 2 + pi;
+	x := 13;
+	s := "Ich bin ein langer String!";
+	c1 := 'a';
+	c2:= 'b';
+	c3 :='c';
+	c4:='d';
+	IF x = 13 THEN
+		f := f + x;
+	ELSE
+		x := x + 1;
+	END;
+	IF x<=15 THEN
+		x := x - 1;
+	END;
 END HelloWorld.
